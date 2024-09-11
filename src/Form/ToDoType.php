4fb036baa2e288f5ae3dd8b92c22936970dd4d99
LeithEng <leithengazzou@gmail.com>
2024-09-11 17:35:17 +0100

    first commit

---
+++ src/Form/ToDoType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ToDo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ToDoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('is_done', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Done',
+                'value'=> true,
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ToDo::class,
+        ]);
+    }
+}
